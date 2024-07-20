6a66c861fe1814548b3ad2b5adfb36121c4de6f5
mathis sergent <sergent.mathis@gmail.com>
2024-07-20 14:37:33 +0200

    ex 2-6, 2-7

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut result = 1;
+
+    for digit in 1..=n {
+        result *= digit
+    }
+
+    result
 }
 
 #[cfg(test)]
